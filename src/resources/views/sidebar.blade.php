72e8cc307fda9722a768950dd6448036e60e7f6e
calvinsinaya <kalvinsinaya04@student.esaunggul.ac.id>
2026-01-31 21:48:04 +0700

    Create sidebar.blade.php

---
+++ src/resources/views/sidebar.blade.php
@@ -0,0 +1,62 @@
+<!--====== SIDEBAR PART START ======-->
+
+<div class="sidebar-left">
+    <div class="sidebar-close">
+        <a class="close" href="#close"><i class="lni lni-close"></i></a>
+    </div>
+
+    <div class="sidebar-content">
+
+        <!-- Deskripsi -->
+        <p class="text">
+            Sistem Manajemen Perpustakaan Digital berbasis web yang dirancang
+            untuk memudahkan pengelolaan buku, kategori, dan peminjaman secara
+            terpusat, efisien, dan aman.
+        </p>
+
+        <!-- Menu -->
+        <div class="sidebar-menu">
+            <h5 class="menu-title">Quick Links</h5>
+            <ul>
+                <li><a class="page-scroll" href="#hero-area">Home</a></li>
+                <li><a class="page-scroll" href="#services">Services</a></li>
+                <li><a class="page-scroll" href="#pricing">Pricing</a></li>
+                <li><a class="page-scroll" href="#latest">Latest Post</a></li>
+                <li><a class="page-scroll" href="#contact">Contact</a></li>
+            </ul>
+        </div>
+
+        <!-- Social -->
+        <div class="sidebar-social align-items-center justify-content-center">
+            <h5 class="social-title">Follow Us On</h5>
+            <ul>
+                <li>
+                    <a href="#" aria-label="Facebook">
+                        <i class="lni lni-facebook-filled"></i>
+                    </a>
+                </li>
+                <li>
+                    <a href="#" aria-label="Twitter">
+                        <i class="lni lni-twitter-original"></i>
+                    </a>
+                </li>
+                <li>
+                    <a href="#" aria-label="LinkedIn">
+                        <i class="lni lni-linkedin-original"></i>
+                    </a>
+                </li>
+                <li>
+                    <a href="#" aria-label="YouTube">
+                        <i class="lni lni-youtube"></i>
+                    </a>
+                </li>
+            </ul>
+        </div>
+
+    </div>
+    <!-- content -->
+</div>
+
+<div class="overlay-left"></div>
+
+<!--====== SIDEBAR PART ENDS ======-->
