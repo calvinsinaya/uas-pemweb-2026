cdaa08d36caf0b04a3a033b355a503aac4f59675
calvinsinaya <kalvinsinaya04@student.esaunggul.ac.id>
2026-01-31 21:36:33 +0700

    Create footer.blade.php

---
+++ src/resources/views/footer.blade.php
@@ -0,0 +1,53 @@
+<footer class="footer-area footer-eleven">
+    <div class="footer-top">
+        <div class="container">
+            <div class="row">
+
+                <!-- Logo & Description -->
+                <div class="col-lg-4">
+                    <img src="{{ asset('assets4/images/logo.svg') }}" alt="Logo" class="mb-3">
+                    <p>
+                        Sistem Manajemen Perpustakaan Digital berbasis web untuk
+                        mempermudah pengelolaan koleksi buku dan peminjaman secara
+                        efisien dan terintegrasi.
+                    </p>
+                    <p>
+                        © {{ date('Y') }} Sistem Perpustakaan Digital
+                    </p>
+                </div>
+
+                <!-- Menu -->
+                <div class="col-lg-2">
+                    <h5>Menu</h5>
+                    <ul>
+                        <li><a class="page-scroll" href="#hero-area">Home</a></li>
+                        <li><a class="page-scroll" href="#services">Services</a></li>
+                        <li><a class="page-scroll" href="#pricing">Pricing</a></li>
+                        <li><a class="page-scroll" href="#contact">Contact</a></li>
+                    </ul>
+                </div>
+
+                <!-- Information -->
+                <div class="col-lg-2">
+                    <h5>Informasi</h5>
+                    <ul>
+                        <li><a href="#">Panduan Perpustakaan</a></li>
+                        <li><a href="#">Keamanan Data</a></li>
+                        <li><a href="#">Kebijakan Privasi</a></li>
+                    </ul>
+                </div>
+
+                <!-- Contact -->
+                <div class="col-lg-4">
+                    <h5>Kontak</h5>
+                    <ul>
+                        <li>Email: admin@perpustakaan.test</li>
+                        <li>Lokasi: Jakarta, Indonesia</li>
+                        <li>Status: Aktif</li>
+                    </ul>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</footer>
