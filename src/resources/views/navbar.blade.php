533073d2acbd9ed76da6e8fdcfe01ed588d387d2
calvinsinaya <kalvinsinaya04@student.esaunggul.ac.id>
2026-01-31 21:44:25 +0700

    Create navbar.blade.php

---
+++ src/resources/views/navbar.blade.php
@@ -0,0 +1,55 @@
+<!--====== NAVBAR NINE PART START ======-->
+
+<section class="navbar-area navbar-nine">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <nav class="navbar navbar-expand-lg">
+
+                    <!-- TOGGLER -->
+                    <button class="navbar-toggler" type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#navbarNine"
+                        aria-controls="navbarNine"
+                        aria-expanded="false"
+                        aria-label="Toggle navigation">
+                        <span class="toggler-icon"></span>
+                        <span class="toggler-icon"></span>
+                        <span class="toggler-icon"></span>
+                    </button>
+
+                    <!-- MENU -->
+                    <div class="collapse navbar-collapse sub-menu-bar" id="navbarNine">
+                        <ul class="navbar-nav me-auto">
+                            <li class="nav-item">
+                                <a class="page-scroll active" href="#hero-area">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="page-scroll" href="#services">Services</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="page-scroll" href="#pricing">Pricing</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="page-scroll" href="#latest">Latest Post</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="page-scroll" href="#contact">Contact</a>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <!-- SIDEBAR BUTTON -->
+                    <div class="navbar-btn d-none d-lg-inline-block">
+                        <a class="menu-bar" href="#side-menu-left">
+                            <i class="lni lni-menu"></i>
+                        </a>
+                    </div>
+
+                </nav>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!--====== NAVBAR NINE PART ENDS ======-->
